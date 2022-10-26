dd449ac4a5354ac3f3428760173552379089c324
Ali E. Huseynov <alieh@code.local>
2022-10-26 12:29:34 +0400

    homework.md file

---
+++ main.js
@@ -0,0 +1,37 @@
+// const obj={ 
+//     name:"Ali",
+//     surname:"Huseynov",
+//     age:18,
+//     hasJob:true
+// }
+// let keys=Object.keys(obj);
+// let values=Object.values(obj);
+// let entries=Object.entries(obj);
+// console.log(keys);
+// console.log(values);
+// console.log(entries);
+
+
+// const arr=[1,2,3,2,1,-1,5,0]
+// let every=arr.every((item)=>item>0);
+// console.log(every);
+// let some=arr.some((item)=>item<0);
+// console.log(some);
+// let includes=arr.includes(1);
+// console.log(includes);
+// let filter=arr.filter((item)=>item%2===0);
+// console.log(filter);
+// let find=arr.find((item)=>item===0);
+// console.log(find);
+// let map=arr.map((item)=>item*2);
+// console.log(map)
+
+
+
+let firstInput=document.getElementsByClassName("first")
+let secondInput=document.getElementsByClassName("second")
+let thirdInput=document.getElementsByClassName("third")
+let button=document.getElementsByClassName("button")
+let arr=[]
+
+
